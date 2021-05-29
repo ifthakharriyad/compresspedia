05f04fc6307b12dac895a0ada88ff0b801cd3d7d
Iftekhar <iftekharriyad@protonmail.com>
2021-05-29 09:55:00 +0600

    edited dropzone style again

--- client/src/components/DropZone.js
+++ client/src/components/DropZone.js
@@ -6,31 +6,34 @@ import Container from '@material-ui/core/Container'
 
 const useStyles = makeStyles(()=>({
     limitSubtitle:{
-        padding:"20px"
+        padding:"20px",
+        color:"#40444f",
+        fontWeight:"400"
     },
     container:{
-        borderColor:"black",
-        width:"375px",
-        height:"165px",
+        border:"2px dashed black",
+        borderRadius:"14px",
         minHeight:"165px",
-        borderRadius:"14px"
+        maxHeight:"165px"
     },
     paragraph:{
         margin: ".8rem .5rem 1.2rem",
-        fontWeight:"600",
-        fontSize:"1.3rem",
-        lineHeight:"2rem"
+        fontWeight:"500",
+        fontSize:"1.2rem",
+        lineHeight:"2rem",
+        color:"black"
     },
     dropZoneContainer:{
-        marginTop:"3em",
-        marginBottom:"3em"
+        marginTop:"3.5em",
+        marginBottom:"3.5em",
+        maxHeight:"165px"
       }
 }))
 
 export default function DropZone(props){
     const classes = useStyles()
     return(
-        <Container id="compress" maxWidth="xs" className={classes.dropZoneContainer}>
+        <Container id="compress" maxWidth="xs" className={classes.dropZoneContainer} fixed>
             <DropzoneArea
                 acceptedFiles={['image/jpeg','image/jpg','image/png', 'image/gif', 'image/svg']}
                 filesLimit={10}
@@ -42,7 +45,7 @@ export default function DropZone(props){
                 dropzoneClass={classes.container}
                 dropzoneParagraphClass={classes.paragraph}
             />
-            <Typography className={classes.limitSubtitle} variant="subtitile1">* Up to 10 images, max 5 MB each.</Typography>
+            <Typography className={classes.limitSubtitle} variant="subtitile2">* Up to 10 images, max 5 MB each.</Typography>
         </Container>
     )
 }
