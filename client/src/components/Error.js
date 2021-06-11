79c986313fdb689a466221fd2022a3d6f0b10065
Iftekhar Riyad <iftekharriyad@protonmail.com>
2021-06-11 19:37:48 +0600

    edited error page style

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import ReplayIcon from '@material-ui/icons/Replay';
+import Container from '@material-ui/core/Container'
 
 const createStyles = makeStyles(theme=>({
     paper:{
@@ -16,12 +17,17 @@ const createStyles = makeStyles(theme=>({
     button:{
         marginTop:"1.5em",
         marginBottom:"1.5em"
-    }
+    },
+    dropZoneContainer:{
+        marginTop:"3em",
+        marginBottom:"3em"
+      }
 }))
 
 export default function Error(props){
     const classes = createStyles()
     return(
+    <Container id="compress" maxWidth="sm" className={classes.dropZoneContainer}>
         <Paper elevation={0} square className={classes.paper}>
             <ErrorOutlineSharpIcon className={classes.errorIcon} color='error'/>
             <Typography variant='h5'>{props.massage}</Typography>
@@ -32,5 +38,6 @@ export default function Error(props){
                 startIcon={<ReplayIcon/>}
             >Try again</Button>
         </Paper>
+    </Container>
     )
 }
